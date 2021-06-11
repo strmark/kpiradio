f3dc3ff82a6949a763604883b364f735c53edcb5
Mark Striekwold <strmark@users.noreply.github.com>
2021-06-11 19:41:09 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlin_version = "1.5.10"
     `version-catalog`
-    id("org.springframework.boot") version "2.5.0"
+    id("org.springframework.boot") version "2.5.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.flywaydb.flyway") version "7.9.2"
     id("com.github.ben-manes.versions") version "0.39.0"
