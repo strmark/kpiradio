ba28d3414b23ac5374329802ec56c1889059d161
Mark Striekwold <strmark@outlook.com>
2022-05-17 22:37:54 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `version-catalog`
     id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.flywaydb.flyway") version "8.5.10"
+    id("org.flywaydb.flyway") version "8.5.11"
     id("com.github.ben-manes.versions") version "0.42.0"
     id("org.sonarqube") version "3.3"
     kotlin("jvm") version kotlinVersion
