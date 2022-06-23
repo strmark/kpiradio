dd60af1ed0d51bcad9e9cf02aea61463035285cd
Mark Striekwold <strmark@outlook.com>
2022-06-23 22:16:26 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = "1.7.0"
     `version-catalog`
-    id("org.springframework.boot") version "2.7.0"
+    id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.flywaydb.flyway") version "8.5.13"
     id("com.github.ben-manes.versions") version "0.42.0"
