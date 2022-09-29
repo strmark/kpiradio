7c54f69a33467c169e310a9c764bf34e92c8dc74
Mark Striekwold <strmark@outlook.com>
2022-09-29 21:00:37 +0200

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.7.10"
+    val kotlinVersion = "1.7.20"
     `version-catalog`
     id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
-    id("org.flywaydb.flyway") version "9.3.1"
+    id("org.flywaydb.flyway") version "9.4.0"
     id("com.github.ben-manes.versions") version "0.42.0"
     id("org.sonarqube") version "3.4.0.2513"
     kotlin("jvm") version kotlinVersion
