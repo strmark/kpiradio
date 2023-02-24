5eebf3cb6d03cc1c014caf37d2963c129137231e
Mark Striekwold <strmark@users.noreply.github.com>
2023-02-24 11:32:17 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,4 +93,11 @@ dependencyCheck {
     analyzers.assemblyEnabled = false
 }
 
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
 apply(plugin = "org.owasp.dependencycheck")
