0b8723f1627af6239c6d081aab7f793ebdb60083
Mark Striekwold <strmark@outlook.com>
2023-03-16 19:26:33 +0100

    version updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     `version-catalog`
     id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.flywaydb.flyway") version "9.15.2"
+    id("org.flywaydb.flyway") version "9.16.0"
     id("com.github.ben-manes.versions") version "0.46.0"
     id("org.sonarqube") version "4.0.0.2929"
     id("org.owasp.dependencycheck") version "8.1.2"
