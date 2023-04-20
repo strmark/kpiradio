e6287ee728714a9dcb4b9a2b51b196831f3207bd
Mark Striekwold <strmark@outlook.com>
2023-04-20 20:04:48 +0200

    version update springboot

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.util.Locale
 plugins {
     val kotlinVersion = "1.8.20"
     `version-catalog`
-    id("org.springframework.boot") version "3.0.5"
+    id("org.springframework.boot") version "3.0.6"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.flywaydb.flyway") version "9.16.3"
     id("com.github.ben-manes.versions") version "0.46.0"
