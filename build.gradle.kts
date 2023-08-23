12efe7ec4b15e08dc4508760eab211ca12f27a81
Mark Striekwold <strmark@outlook.com>
2023-08-23 21:50:51 +0200

    tweaked for jetty

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.ir.backend.js.compile
 
 plugins {
     with(kpiLibs.versions) {
@@ -27,13 +26,11 @@ allprojects {
 
     with(kpiLibs.versions) {
         dependencies {
-//            ext["jakarta-servlet.version"] = jakarta.get()
+            ext["jakarta-servlet.version"] = jakarta.get()
             implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jackson.get()}")
             implementation("com.h2database:h2:${h2db.get()}")
             implementation("io.github.microutils:kotlin-logging:${klogging.get()}")
             implementation("org.flywaydb:flyway-core:${flyway.get()}")
-//            runtimeOnly("jakarta.servlet:jakarta.servlet-api:${jakarta.get()}")
-            compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
             implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlin.get()}")
             implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlin.get()}")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${kotlinx.get()}")
@@ -43,12 +40,13 @@ allprojects {
             implementation("org.springframework.boot:spring-boot-starter-validation:${springboot.get()}")
             implementation("org.springframework.boot:spring-boot-starter-web:${springboot.get()}")
             implementation("org.springframework.boot:spring-boot-starter-jetty:${springboot.get()}")
-//            modules {
-//                module("org.springframework.boot:spring-boot-starter-tomcat") {
-//                    replacedBy("org.springframework.boot:spring-boot-starter-jetty", "Use Jetty instead of Tomcat")
-//                }
-//            }
-//            compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+            modules {
+                module("org.springframework.boot:spring-boot-starter-tomcat") {
+                    replacedBy("org.springframework.boot:spring-boot-starter-jetty", "Use Jetty instead of Tomcat")
+                }
+            }
+            runtimeOnly("jakarta.servlet:jakarta.servlet-api:${jakarta.get()}")
+            testImplementation("jakarta.servlet:jakarta.servlet-api:${jakartatest.get()}")
             implementation("org.yaml:snakeyaml:${snakeyaml.get()}")
             developmentOnly("org.springframework.boot:spring-boot-devtools")
             implementation("org.springdoc:springdoc-openapi-kotlin-tests:${swagger.get()}")
