98f572e4b3a03d8330ab0e33721e3b5bba6a6ec5
Mark Striekwold <strmark@outlook.com>
2023-11-20 21:42:42 +0100

    changed to jdk version 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 allprojects {
     group = "nl.strmark"
     version = "0.0.1-SNAPSHOT"
-    java.sourceCompatibility = JavaVersion.VERSION_17
+    java.sourceCompatibility = JavaVersion.VERSION_21
 
     repositories {
         mavenCentral()
@@ -59,7 +59,7 @@ allprojects {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
